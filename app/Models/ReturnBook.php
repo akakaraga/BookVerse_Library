47451781b3a793de2cb2740370cda7f9b1c11ee7
helgaaka <helga.aka@gmail.com>
2025-10-23 10:40:49 +0700

    update return book migration

---
+++ app/Models/ReturnBook.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Enums\ReturnBookStatus;
+use App\Models\Loan;
+use App\Models\Book;
+use App\Models\Fine;
+use App\Models\ReturnBookCheck;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class ReturnBook extends Model
+{
+    protected $fillable = [
+        'return_book_code',
+        'loan_id',
+        'user_id',
+        'book_id',
+        'return_date',
+        'status',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'return_date' => 'date',
+            'status' => ReturnBookStatus::class,
+        ];
+    }
+
+    public function loan(): BelongsTo
+    {
+        return $this->belongsTo(Loan::class);
+    }
+
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function fine(): HasOne
+    {
+        return $this->hasOne(Fine::class);
+    }
+
+    public function returnBookCheck():HasOne
+    {
+        return $this->hasOne(ReturnBookCheck::class);
+    }
+}
